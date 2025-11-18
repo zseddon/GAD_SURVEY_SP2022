fb86bbadf3ee56b494f154e6653aa990aecf745b
Zach Seddon <seddonz1@outlook.com>
2025-11-18 14:51:42 -0500

    Create gad_survey table for survey data

---
+++ schema/create_tables.sql
@@ -0,0 +1,11 @@
+CREATE TABLE gad_survey (
+    "respondent_id" INTEGER,
+    "age" INTEGER,
+    "gender" TEXT,
+    "education_level" TEXT,
+    "income" INTEGER,
+    "gad_score" INTEGER,
+    "diagnosis" TEXT,
+    "country" TEXT,
+    "timestamp" TEXT
+);
